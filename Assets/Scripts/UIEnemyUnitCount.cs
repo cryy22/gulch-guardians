b7e965d2436c76b5780e04583464002ba499c002
cryy22 <cry@anon-f77397bb01ed4c01.art>
2022-11-03 17:20:13 -0700

    add counter for number of remaining enemy units

---
+++ Assets/Scripts/UIEnemyUnitCount.cs
@@ -0,0 +1,29 @@
+using System;
+using GulchGuardians;
+using TMPro;
+using UnityEngine;
+
+public class UIEnemyUnitCount : MonoBehaviour
+{
+    [SerializeField] private Team EnemyTeam;
+
+    [SerializeField] private TMP_Text CounterText;
+    [SerializeField] private TMP_Text SuffixText;
+
+    private void OnEnable()
+    {
+        EnemyTeam.UnitsChanged += UnitsChangedEventHandler;
+        UnitsChangedEventHandler(sender: this, e: EventArgs.Empty);
+    }
+
+    private void OnDisable() { EnemyTeam.UnitsChanged -= UnitsChangedEventHandler; }
+
+    private void UnitsChangedEventHandler(object sender, EventArgs e) { UpdateCounter(); }
+
+    private void UpdateCounter()
+    {
+        int unitCount = EnemyTeam.Units.Count;
+        CounterText.text = unitCount.ToString();
+        SuffixText.text = unitCount == 1 ? "enemy unit remaining" : "enemy units remaining";
+    }
+}
