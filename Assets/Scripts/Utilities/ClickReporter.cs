1308f037cf462a2e67fd5dd3f8942886a65ca9ec
cryy22 <cry@anon-f77397bb01ed4c01.art>
2022-11-03 17:20:16 -0700

    BUGFIX: fix new input system clicks on sprites with colliders

--- Assets/Scripts/Utilities/ClickReporter.cs
+++ Assets/Scripts/Utilities/ClickReporter.cs
@@ -1,15 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Object = UnityEngine.Object;
 
 namespace GulchGuardians
 {
     [RequireComponent(typeof(Collider2D))]
-    public class ClickReporter : MonoBehaviour
+    public class ClickReporter : MonoBehaviour, IPointerClickHandler
     {
         public Object Sender;
         public event EventHandler Clicked;
 
-        private void OnMouseDown() { Clicked?.Invoke(sender: Sender, e: EventArgs.Empty); }
+        public void OnPointerClick(PointerEventData eventData) { Clicked?.Invoke(sender: Sender, e: EventArgs.Empty); }
     }
 }
