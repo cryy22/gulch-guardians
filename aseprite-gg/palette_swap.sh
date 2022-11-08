0d3cd19d18d280d87a40deca6cd08041d4eb6b43
cryy22 <cry@anon-f77397bb01ed4c01.art>
2022-11-08 13:52:12 -0800

    wip - add hurt frames to SpriteLibraryAssets

--- aseprite-gg/palette_swap.sh
+++ aseprite-gg/palette_swap.sh
@@ -1,18 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-aseprite -b ./templates/TEMPLATE-gg-player.aseprite --palette ./palettes/gg-palette-player_green.aseprite --save-as ./sprites/gg-player-green.aseprite
-aseprite -b ./templates/TEMPLATE-gg-player.aseprite --palette ./palettes/gg-palette-player_sage.aseprite --save-as ./sprites/gg-player-sage.aseprite
-aseprite -b ./templates/TEMPLATE-gg-player.aseprite --palette ./palettes/gg-palette-player_yellow.aseprite --save-as ./sprites/gg-player-yellow.aseprite
-aseprite -b ./templates/TEMPLATE-gg-player.aseprite --palette ./palettes/gg-palette-player_purple.aseprite --save-as ./sprites/gg-player-purple.aseprite
+for color in green sage yellow purple
+do
+	aseprite -b ./templates/TEMPLATE-gg-player.aseprite --palette "./palettes/gg-palette-player_$color.aseprite" --save-as "./sprites/gg-player-$color.aseprite"
+done
 
-aseprite -b ./templates/TEMPLATE-gg-enemy_grunt.aseprite --palette ./palettes/gg-palette-enemy_grunt_green.aseprite --save-as ./sprites/gg-enemy_grunt-green.aseprite
-aseprite -b ./templates/TEMPLATE-gg-enemy_grunt.aseprite --palette ./palettes/gg-palette-enemy_grunt_beige.aseprite --save-as ./sprites/gg-enemy_grunt-beige.aseprite
-aseprite -b ./templates/TEMPLATE-gg-enemy_grunt.aseprite --palette ./palettes/gg-palette-enemy_grunt_blue.aseprite --save-as ./sprites/gg-enemy_grunt-blue.aseprite
+for color in green beige blue
+do
+	aseprite -b ./templates/TEMPLATE-gg-enemy_grunt.aseprite --palette "./palettes/gg-palette-enemy_grunt_$color.aseprite" --save-as "./sprites/gg-enemy_grunt-$color.aseprite"
+done
 
-aseprite -b ./templates/TEMPLATE-gg-enemy_knight.aseprite --palette ./palettes/gg-palette-enemy_knight_purple.aseprite --save-as ./sprites/gg-enemy_knight-purple.aseprite
-aseprite -b ./templates/TEMPLATE-gg-enemy_knight.aseprite --palette ./palettes/gg-palette-enemy_knight_green.aseprite --save-as ./sprites/gg-enemy_knight-green.aseprite
-aseprite -b ./templates/TEMPLATE-gg-enemy_knight.aseprite --palette ./palettes/gg-palette-enemy_knight_yellow.aseprite --save-as ./sprites/gg-enemy_knight-yellow.aseprite
+for color in green yellow purple
+do
+	aseprite -b ./templates/TEMPLATE-gg-enemy_knight.aseprite --palette "./palettes/gg-palette-enemy_knight_$color.aseprite" --save-as "./sprites/gg-enemy_knight-$color.aseprite"
+done
 
-aseprite -b ./templates/TEMPLATE-gg-enemy_stilter.aseprite --palette ./palettes/gg-palette-enemy_stilter_purple.aseprite --save-as ./sprites/gg-enemy_stilter-purple.aseprite
-aseprite -b ./templates/TEMPLATE-gg-enemy_stilter.aseprite --palette ./palettes/gg-palette-enemy_stilter_yellow.aseprite --save-as ./sprites/gg-enemy_stilter-yellow.aseprite
-aseprite -b ./templates/TEMPLATE-gg-enemy_stilter.aseprite --palette ./palettes/gg-palette-enemy_stilter_blue.aseprite --save-as ./sprites/gg-enemy_stilter-blue.aseprite
+for color in yellow
+do
+	aseprite -b ./templates/TEMPLATE-gg-enemy_knight_boss.aseprite --palette "./palettes/gg-palette-enemy_knight_boss_$color.aseprite" --save-as "./sprites/gg-enemy_knight_boss-$color.aseprite"
+done
+
+for color in purple yellow blue
+do
+	aseprite -b ./templates/TEMPLATE-gg-enemy_stilter.aseprite --palette "./palettes/gg-palette-enemy_stilter_$color.aseprite" --save-as "./sprites/gg-enemy_stilter-$color.aseprite"
+done
