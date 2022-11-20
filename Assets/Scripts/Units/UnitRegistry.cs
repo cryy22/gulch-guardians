832316ab1f50e35a22e7d3d304fd65ace983b372
cryy22 <cry@anon-f77397bb01ed4c01.art>
2022-11-20 04:37:00 -0800

    update Registry for crysc pattern change

--- Assets/Scripts/Units/UnitRegistry.cs
+++ Assets/Scripts/Units/UnitRegistry.cs
@@ -1,4 +1,3 @@
-using System;
 using Crysc.Registries;
 using UnityEngine;
 
@@ -6,11 +5,5 @@ namespace GulchGuardians.Units
 {
     [CreateAssetMenu(fileName = "UnitRegistry", menuName = "Registries/Unit")]
     public class UnitRegistry : Registry<Unit>
-    {
-        public event EventHandler Destroying;
-
-        protected override void SubscribeToEvents(Unit unit) { unit.Destroying += DestroyingEventHandler; }
-        protected override void UnsubscribeFromEvents(Unit unit) { unit.Destroying -= DestroyingEventHandler; }
-        private void DestroyingEventHandler(object sender, EventArgs e) { Destroying?.Invoke(sender: sender, e: e); }
-    }
+    { }
 }
