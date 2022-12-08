628e320ab577ab65df41da5be8a4e9e60003ea00
cryy22 <cry@anon-f77397bb01ed4c01.art>
2022-12-08 00:21:45 -0800

    display confirmation before restarting game

--- Assets/Scripts/SceneControls/UIRestartButton.cs
+++ Assets/Scripts/SceneControls/UIRestartButton.cs
@@ -1,4 +1,5 @@
 using GulchGuardians.Constants;
+using GulchGuardians.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,14 +7,18 @@ using UnityEngine.UI;
 namespace GulchGuardians.SceneControls
 {
     [RequireComponent(typeof(Button))]
-    public class UIResetButton : MonoBehaviour
+    public class UIRestartButton : MonoBehaviour
     {
+        [SerializeField] private UIConfirmationOverlay ConfirmationOverlay;
+
         private Button _button;
 
         private void Awake() { _button = GetComponent<Button>(); }
         private void OnEnable() { _button.onClick.AddListener(OnButtonClicked); }
         private void OnDisable() { _button.onClick.RemoveListener(OnButtonClicked); }
 
-        private static void OnButtonClicked() { SceneManager.LoadScene(Scenes.MainIndex); }
+        private void OnButtonClicked() { ConfirmationOverlay.Show(onConfirm: RestartAction, onCancel: CancelAction); }
+        private void RestartAction() { SceneManager.LoadScene(Scenes.MainIndex); }
+        private void CancelAction() { gameObject.SetActive(true); }
     }
 }
--- Assets/Scripts/UI/UIConfirmationOverlay.cs
+++ Assets/Scripts/UI/UIConfirmationOverlay.cs
@@ -1,11 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace GulchGuardians.UI
 {
     public class UIConfirmationOverlay : MonoBehaviour
     {
+        [SerializeField] private GameObject Container;
         [SerializeField] private Button ConfirmButton;
         [SerializeField] private Button CancelButton;
+
+        private void Awake() { Container.SetActive(false); }
+
+        public void Show(Action onConfirm, Action onCancel)
+        {
+            Container.SetActive(true);
+            ConfirmButton.onClick.AddListener(SelectionResponse(onConfirm));
+            CancelButton.onClick.AddListener(SelectionResponse(onCancel));
+        }
+
+        private UnityAction SelectionResponse(Action callback)
+        {
+            return () =>
+            {
+                callback?.Invoke();
+                Container.SetActive(false);
+            };
+        }
     }
 }
