4a423c1dcd189dba4b121e5750d33acef0e5e696
cryy22 <cry@anon-f77397bb01ed4c01.art>
2022-12-07 23:57:53 -0800

    wip - bootstrap confirmation overlay

---
+++ Assets/Scripts/UI/UIConfirmationOverlay.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GulchGuardians.UI
+{
+    public class UIConfirmationOverlay : MonoBehaviour
+    {
+        [SerializeField] private Button ConfirmButton;
+        [SerializeField] private Button CancelButton;
+    }
+}
