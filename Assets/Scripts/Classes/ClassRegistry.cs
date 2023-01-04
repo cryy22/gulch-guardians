2ccac5ce5c78f3f1ba0980cec17bc68ad44a5af2
cryy22 <cry@anon-f77397bb01ed4c01.art>
2023-01-04 05:54:14 -0800

    failed attempt at using instance pattern to fetch Registries

--- Assets/Scripts/Classes/ClassRegistry.cs
+++ Assets/Scripts/Classes/ClassRegistry.cs
@@ -1,9 +1,14 @@
+using Crysc.Common;
 using Crysc.Registries;
+using GulchGuardians.Constants;
 using UnityEngine;
 
 namespace GulchGuardians.Classes
 {
     [CreateAssetMenu(fileName = "ClassRegistry", menuName = "Registries/Class")]
     public class ClassRegistry : Registry<ClassType>
-    { }
+    {
+        private static readonly InstanceCacher<ClassRegistry> _cacher = new(AssetAddresses.ClassRegistry);
+        private static ClassRegistry I => _cacher.I;
+    }
 }
